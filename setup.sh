f36f7ab6977d7458b1e474ba0c6ea885d16740f3
yamatatsu <the.kinnikuman@gmail.com>
2020-10-13 18:38:15 +0900

    deploying

--- setup.sh
+++ setup.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# use installed fish
-sudo chsh -s /usr/bin/fish
+# # use installed fish
+# sudo chsh -s /usr/bin/fish
 
-# apt-get
-sudo apt-get update
-sudo apt-get install -y \
-  groff \
+# # apt-get
+# sudo apt-get update
+# sudo apt-get install -y \
+#   groff \
 
-# install tools
-for shfile in ./install/* ;do
-  bash ${shfile}
-done
-
-# TODO: deploy files
-# for f in .??*
-# do
-#     [[ "$f" == ".git" ]] && continue
-#     [[ "$f" == ".DS_Store" ]] && continue
-
-#     echo "$f"
+# echo '===== install tools ====='
+# for shfile in ./install/* ;do
+#   bash ${shfile}
 # done
+
+echo '===== deploy files ====='
+cd ./deploy
+for path in .??* ;do
+  echo ${path}
+  ln -sf $(realpath ${path}) ~/${path}
+done
